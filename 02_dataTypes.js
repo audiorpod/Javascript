e9b03b3cbdf9d8fbb3f253baf59768892205d42b
Rupesh Sahu <91730407+audiorpod@users.noreply.github.com>
2023-11-14 12:27:09 +0000

    datatypes

--- 02_dataTypes.js
+++ 02_dataTypes.js
@@ -4,6 +4,23 @@
 previously we use this in console and its provide
  pop-up and that is sum 6 but now we dont use  this old way */ 
 
- 
+
+let name = " rupesh "         // -> string 
+let age = 21                  // -> number 
+let isLoggedIn =  false       // -> boolean 
 
 
+/* number => 2 to powe 53  ( range )
+   bigint ( use if noexceed 2 Power 53 ) majorly use in stock , trading etc 
+   string => " " ( use in double quated its good practice )
+   boolen => true or false 
+   null => standlalone ( ya stand alone value hai ) representation of empty value 
+   undefined => variable declare kiya hai but vaue abhi tak diya nai hai 
+   symbol => Unique ( if we want any unique value we use symbol )majorly we will use react , figma etc )
+   object 
+
+*/
+
+// INTERVIEW QUESTION : NULL IN JAVASCRIPT IS OBJECT 
+console.log(typeof undefined); // output : => Undefined 
+console.log(typeof null); // output :=> object 
